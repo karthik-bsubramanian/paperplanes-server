71e7439ed59d1ad0d2e37d06bc623b2403dd926f
Karthik Balasubramanian <karthikbalasubramanian08@gmail.com>
2025-05-27 22:37:32 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import { Hono } from 'hono'
 import user from './routes/user';
 import blog from './routes/blog';
+import { cors } from 'hono/cors';
 
 const app = new Hono().basePath("api/v1");
+
+app.use('*', cors())
+app.use(
+  '*',
+  cors({
+    origin: 'http://localhost:5173',
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests','Authorization'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  })
+)
 
 app.route("/user",user);
 
